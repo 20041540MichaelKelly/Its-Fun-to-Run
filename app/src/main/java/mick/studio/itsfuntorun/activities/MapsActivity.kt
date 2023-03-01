047a08b5e771a054c5950d47802fa19b7ed32831
Michael Kelly <mikelly1988@gmail.com>
2023-03-01 22:25:34 +0000

    Implemented Live tracking now through onLocationChanged

--- app/src/main/java/mick/studio/itsfuntorun/activities/MapsActivity.kt
+++ app/src/main/java/mick/studio/itsfuntorun/activities/MapsActivity.kt
@@ -9,26 +9,31 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.LocationSource.OnLocationChangedListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.android.gms.tasks.CancellationTokenSource
 import mick.studio.itsfuntorun.R
 import mick.studio.itsfuntorun.databinding.ActivityMapsBinding
 import mick.studio.itsfuntorun.models.Location
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     GoogleMap.OnMarkerDragListener,
-    GoogleMap.OnMarkerClickListener,
-    OnLocationChangedListener{
+    GoogleMap.OnMarkerClickListener{
+    var locationRequest = createLocationRequest()
+
+    var requestingLocationUpdates = false
+    var isMapReady = false
+    val polylineOptions = PolylineOptions()
+
+    // globally declare LocationCallback
+    private lateinit var locationCallback: LocationCallback
 
     //2
     private val locations = mutableListOf<LatLng>()
@@ -37,13 +42,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     private lateinit var binding: ActivityMapsBinding
     private var location = Location()
     private var listOfLatLng = mutableListOf<LatLng>()
-    private val LOCATION_REQUEST_CODE = 101
-
-    private var currentLocation: Location? = null
-
-
-    //This allows an option for the user to cancel
-    private var cancellationTokenSource = CancellationTokenSource()
+    private var LOCATION_REQUEST_CODE = 101
+    private var myMarker : Marker? = null
 
     /**
      * A Lazy method is used here so it is only called when needed
@@ -57,63 +57,84 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         super.onCreate(savedInstanceState)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //location = intent.extras?.getParcelable<Location>("location")!!
-        // TODO: Step 1.3, Create a LocationRequest.
+        getContinousLocation()
 
+        //location = intent.extras?.getParcelable<Location>("location")
         //No.2
-        if(checkUserPermission()) { // I call this to ensure that permission have been accepted
-            getLocation()
+        if (checkUserPermission()) { // I call this to ensure that permission have been accepted
+            mapSetup()
+            startLocationUpdates()
+        }
+    }
+
+    private fun getContinousLocation() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    myMarker?.position = LatLng(location.latitude, location.longitude)
+                    map.moveCamera(
+                        CameraUpdateFactory.newLatLng(
+                            LatLng(
+                                location.latitude,
+                                location.longitude
+                            )
+                        )
+                    )
+                    drawRoute(LatLng(location.latitude, location.longitude))
+                }
+            }
         }
     }
 
     //No.5
     override fun onMapReady(googleMap: GoogleMap) {
         val lat_lng = LatLng(location.lat, location.lng)
-
         map = googleMap
+        moveMarker(lat_lng)
+    }
+
+        //override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+    private fun moveMarker(latLng: LatLng) {
         val options = MarkerOptions()
             .title("Run")
-            .snippet("GPS : $lat_lng")
+            .snippet("GPS : $latLng")
             .draggable(true)
-            .position(lat_lng)
+            .position(latLng)
             .icon(bitmapDescriptorFromVector(this, R.drawable.baseline_directions_run_24))
-        map.addMarker(options)
+
+        myMarker = map.addMarker(options)
         map.setOnMarkerDragListener(this)
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(lat_lng, location.zoom))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, location.zoom))
     }
 
     //No.3
     private fun getLocation() {
-        //checkUserPermission()
         mapSetup()
     }
 
     //No.4
     @SuppressLint("MissingPermission")
     private fun mapSetup() {
-        /*fusedLocationClient.getCurrentLocation(
-            PRIORITY_HIGH_ACCURACY,
-            cancellationTokenSource.token
-        )*/
         fusedLocationClient.lastLocation.addOnSuccessListener { task ->
-                location.lat = task.latitude
-                location.lng = task.longitude
-                location.zoom = 15f
+            location.lat = task.latitude
+            location.lng = task.longitude
+            location.zoom = 15f
 
-                locations.add(LatLng(task.latitude, task.longitude))
-                setUptheMap()
-            }
-
+            locations.add(LatLng(task.latitude, task.longitude))
+            setUpTheMap()
+        }
     }
 
-    private fun setUptheMap() {
+    private fun setUpTheMap() {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        isMapReady = true
     }
 
     //No.3
-    fun checkUserPermission() : Boolean {
+    fun checkUserPermission(): Boolean {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -122,13 +143,50 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
-            return true
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
+            return false
         }
-        return false
+        requestingLocationUpdates= true
+        return true
     }
-//No.2 after the permission was granted by the user
+
+    //first bp
+    fun createLocationRequest() : LocationRequest {
+         val locationRequest = LocationRequest.create()
+        //Instantiating the Location request and setting the priority and the interval I need to update the location.
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(500);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        return locationRequest
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (requestingLocationUpdates && isMapReady) startLocationUpdates()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+            locationCallback,
+            Looper.getMainLooper())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    //No.2 after the permission was granted by the user
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -136,11 +194,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when(requestCode) {
-            LOCATION_REQUEST_CODE -> if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLocation()
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mapSetup()
+                startLocationUpdates()
             }
-
         }
     }
 
@@ -164,8 +222,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         location.lat = marker.position.latitude
         location.lng = marker.position.longitude
         location.zoom = map.cameraPosition.zoom
-
-        drawRoute(listOfLatLng)
     }
 
     override fun onMarkerDragStart(marker: Marker) {
@@ -175,13 +231,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         listOfLatLng.add(LatLng(marker.position.latitude, marker.position.longitude))
     }
 
-    private fun drawRoute(lat_lng: MutableList<LatLng>) {
-        val polylineOptions = PolylineOptions()
-
-        val points = polylineOptions.points
-        points.addAll(lat_lng)
-
-        map.addPolyline(polylineOptions)
+    private fun drawRoute(lat_lng: LatLng) {
+        polylineOptions.points+=lat_lng
+        if(isMapReady){ map.addPolyline(polylineOptions)}
     }
 
     override fun onMarkerClick(marker: Marker): Boolean {
@@ -200,8 +252,4 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         finish()
         super.onBackPressed()
     }
-
-    override fun onLocationChanged(loc: android.location.Location) {
-
-    }
- }
+}
